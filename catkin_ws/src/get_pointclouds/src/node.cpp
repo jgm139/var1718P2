714ba63bd63fc6be6aebdbd5196fff4f6f97570b
Julia García Martínez <jgm139@alu.ua.es>
2018-05-07 20:48:34 +0200

    Modelado alieando y creado correctamente, faltan features globales

--- catkin_ws/src/get_pointclouds/src/node.cpp
+++ catkin_ws/src/get_pointclouds/src/node.cpp
@@ -4,28 +4,15 @@
 #include <boost/foreach.hpp>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/visualization/cloud_viewer.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/keypoints/sift_keypoint.h>
-#include <pcl/kdtree/kdtree.h>
 #include <pcl/io/pcd_io.h>
-#include <pcl/impl/point_types.hpp>
-#include <pcl/features/board.h>
-#include <pcl/features/shot_omp.h>
-#include <pcl/features/normal_3d_omp.h>
-#include <pcl/sample_consensus/sac_model_sphere.h>
-#include <pcl/sample_consensus/ransac.h>
-#include <pcl/registration/icp.h>
-#include <pcl/registration/ia_ransac.h>
-#include <pcl/registration/correspondence_estimation.h>
-#include <pcl/keypoints/iss_3d.h>
 #include <vector>
+#include <sstream>
+#include <fstream>
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr visu_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
 std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr > clouds;
-std::vector<pcl::PointCloud<pcl::SHOT352>::Ptr > descriptors;
-std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr > keypoints;
 bool capture = false;
-bool align = false;
+const char* menu = "Pulsa:\n\ts: Para guardar todo el conjunto de nubes.\n\tb: Para borrar la última nube capturada.\n\tCualquier otra tecla (menos enter): Para capturar una nube de puntos.\n"; 
 
 void simpleVis (){
   	pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
@@ -35,53 +22,34 @@ void simpleVis (){
 		boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
 	}
 }
-double computeCloudResolution(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& cloud){
-	double resolution = 0.0;
-	int numberOfPoints = 0;
-	int nres;
-	std::vector<int> indices(2);
-	std::vector<float> squaredDistances(2);
-	pcl::search::KdTree<pcl::PointXYZRGB> tree;
-	tree.setInputCloud(cloud);
-
-	for (size_t i = 0; i < cloud->size(); ++i)
-	{
-		if (! pcl_isfinite((*cloud)[i].x))
-			continue;
-
-		// Considering the second neighbor since the first is the point itself.
-		nres = tree.nearestKSearch(i, 2, indices, squaredDistances);
-		if (nres == 2)
-		{
-			resolution += sqrt(squaredDistances[1]);
-			++numberOfPoints;
-		}
+void saveCloudPoints(){
+	ofstream f;
+	f.open("listOfPointClouds.txt");
+	for(unsigned int i = 0;i<clouds.size();i++){
+		std::stringstream ss;
+		ss << i;
+		f << "pointsCloudsFiles/cloud"+ss.str()+".pcd" << '\n';
+		pcl::io::savePCDFileBinary("pointsCloudsFiles/cloud"+ss.str()+".pcd", *clouds[i]);
+		std::cerr << "Saved " << (*clouds[i]).size() << " data points to pointsCloudsFiles/cloud"+ss.str()+".pcd" << std::endl;
 	}
-	if (numberOfPoints != 0)
-		resolution /= numberOfPoints;
-
-	return resolution;
+	f.close();
 }
 
-void saveCloudPoints(){
-	for(unsigned int i = 0;i<clouds.size();i++){
-		pcl::io::savePCDFileBinary("test_pcd.pcd", *clouds[i]);
-		std::cerr << "Saved " << (*clouds[i]).size() << " data points to test_pcd.pcd." << std::endl;
-	}
+void deleteLastOne(){
+	clouds.pop_back();
 }
 
 void captureImage(){
 	while(true){
-		std::cout << "Pulsar para capturar nube de puntos e incluirla al vector" << std::endl;
-		char option = getchar(); 
-		switch(option){
-			case 'a':
-				align = true;
-				break;
+		switch(getchar()){
 			case 's':
 				saveCloudPoints();
 				break;
+			case 'b':
+				deleteLastOne();
+				break;
 			case 10: //Enter, no hacer nada
+				std::cout << menu;
 				break;
 			default:
 				capture = true;
@@ -90,174 +58,12 @@ void captureImage(){
 	}
 }
 
-void calculateKeyPoints(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& keypoints){
-		// ISS keypoint detector object.
-		pcl::ISSKeypoint3D<pcl::PointXYZRGB, pcl::PointXYZRGB> detector;
-		detector.setInputCloud(cloud);
-		pcl::search::KdTree<pcl::PointXYZRGB>::Ptr kdtree(new pcl::search::KdTree<pcl::PointXYZRGB>);
-		detector.setSearchMethod(kdtree);
-		
-		double resolution = computeCloudResolution(cloud);
-		// Set the radius of the spherical neighborhood used to compute the scatter matrix.
-		detector.setSalientRadius(6 * resolution);
-		// Set the radius for the application of the non maxima supression algorithm.
-		detector.setNonMaxRadius(4 * resolution);
-		// Set the minimum number of neighbors that has to be found while applying the non maxima suppression algorithm.
-		detector.setMinNeighbors(5);
-		// Set the upper bound on the ratio between the second and the first eigenvalue.
-		detector.setThreshold21(0.975);
-		// Set the upper bound on the ratio between the third and the second eigenvalue.
-		detector.setThreshold32(0.975);
-		// Set the number of prpcessing threads to use. 0 sets it to automatic.
-		detector.setNumberOfThreads(4);
-		detector.compute(*keypoints);
-}
-
-// Using NormalFeature
-void calculateFeatures(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud_filtered, pcl::PointCloud<pcl::Normal>::Ptr& cloud_normals){
-	
-	// Create the normal estimation class, and pass the input dataset to it
-	pcl::NormalEstimationOMP<pcl::PointXYZRGB, pcl::Normal> ne;
-	ne.setInputCloud (cloud_filtered);
-	// Pass the original data (before downsampling) as the search surface
-	ne.setSearchSurface (cloud);
-
-	// Create an empty kdtree representation, and pass it to the normal estimation object.
-	// Its content will be filled inside the object, based on the given surface dataset.
-	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB> ());
-	ne.setSearchMethod (tree);
-
-	// Use all neighbors in a sphere of radius 3cm
-	ne.setRadiusSearch (0.07);
-
-	// Compute the features
-	ne.compute (*cloud_normals);
-}
-
-
-void ransac/*?*/(){
-	// Método de mío - Sin testear
-	/*std::vector<int> inliers;
-
-  	pcl::SampleConsensusModelSphere<pcl::PointXYZRGB>::Ptr model_s(new pcl::SampleConsensusModelSphere<pcl::PointXYZRGB> (cloud));
-  	pcl::RandomSampleConsensus<pcl::PointXYZRGB> ransac(model_s);
-    ransac.setDistanceThreshold (.01);
-    ransac.computeModel();
-    ransac.getInliers(inliers);
-
-    pcl::copyPointCloud<pcl::PointXYZRGB>(*cloud, inliers, *final);*/
-}
-
-/*void matchFeatures(){
-	pcl::CorrespondenceEstimation<pcl::Normal, pcl::Normal> est;
-	est.setInputCloud(source_features);
-	est.setInputTarget(target_features);
-	est.determineCorrespondences(correspondences);
-}*/
-// Using Sphere model
-void performAlign(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& input_cloud, 
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr& target_cloud, pcl::PointCloud<pcl::SHOT352>::Ptr& source_features, 
-	pcl::PointCloud<pcl::SHOT352>::Ptr& target_features, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& aligned_cloud, 
-	Eigen::Matrix4f& transformation){
-
-	
-    // Método de mío
-	pcl::IterativeClosestPoint<pcl::PointXYZRGB, pcl::PointXYZRGB> icp;
-	icp.setInputSource (input_cloud);
-	icp.setInputTarget (target_cloud);
-	icp.align (*aligned_cloud);
-
-	transformation = icp.getFinalTransformation ();
-    
-
-    // Método de mío - Refinar (supuestos buenos resultados)
-    /*pcl::SampleConsensusInitialAlignment<pcl::PointXYZRGB, pcl::PointXYZRGB, pcl::SHOT352> sac_ia;
-	sac_ia.setNumberOfSamples (3);
-	sac_ia.setMinSampleDistance (0.07f);
-	sac_ia.setCorrespondenceRandomness (5);
-	sac_ia.setMaximumIterations (5000);
-	sac_ia.setInputSource (input_cloud);
-	sac_ia.setInputTarget (target_cloud);
-	sac_ia.setSourceFeatures (source_features);
-	sac_ia.setTargetFeatures (target_features);
-	sac_ia.align(*aligned_cloud);
-	transformation = sac_ia.getFinalTransformation ();*/
-
-	//std::vector<int> inliers;
-	/* Método de david
-    registration::CorrespondenceRejectorSampleConsensus<pcl::PointXYZRGB> sac;
-    sac.setInputSource(keypoints_2); // reverse? (2 for 1) 
-    sac.setInputTarget(keypoints_1); // reverse (1 for 2) 
-    sac.setInlierThreshold(0.07); // Too much? default 0.01 
-    sac.setMaximumIterations(2000); //default 1000 
-    sac.setRefineModel(false); // default false 
-    sac.setInputCorrespondences(correspondences); 
-    sac.getCorrespondences(*inliers); 
-    Eigen::Matrix4f transformation = refiner.getBestTransformation();
-	*/
-
-    //pcl::transformPointCloud (*input_cloud, *transformed_cloud, transformation);
-
-}
-
-void calculateDescriptors(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& keypoints,
- pcl::PointCloud<pcl::Normal>::Ptr& cloud_normals, pcl::PointCloud<pcl::SHOT352>::Ptr& result){
-
-	pcl::SHOTEstimationOMP<pcl::PointXYZRGB, pcl::Normal, pcl::SHOT352> descr_est;
-    descr_est.setRadiusSearch (0.07f); //descr_rad
-	descr_est.setInputCloud (keypoints);
-	descr_est.setInputNormals (cloud_normals);
-	descr_est.setSearchSurface (cloud);
-	descr_est.compute (*result);
-}
-
 void callback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg){
 	if(capture){
 		capture = false;
 		pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>(*msg));
-		pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
-
-		// Filtramos los puntos
-		pcl::VoxelGrid<pcl::PointXYZRGB > vGrid;
-		vGrid.setInputCloud (cloud);
-		vGrid.setLeafSize (0.05f, 0.05f, 0.05f);
-		vGrid.filter (*cloud_filtered);
-
-		std::cout << "Cloud: " << cloud_filtered->size();
-
-		// Conseguimos los KeyPoints
-		pcl::PointCloud<pcl::PointXYZRGB>::Ptr keypoint(new pcl::PointCloud<pcl::PointXYZRGB>);
-		calculateKeyPoints(cloud_filtered, keypoint);
-		std::cout << "KeyPoints: " << keypoint->size();
-		// Calculamos las Features
-		/// Mirar otras que no sean normales///
-		pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
-		calculateFeatures(cloud, cloud_filtered, cloud_normals);
-		cout << "Normal Clouds: " << cloud_normals->size();
-
-		// Calculamos los descriptores
-		pcl::PointCloud<pcl::SHOT352>::Ptr descriptor (new pcl::PointCloud<pcl::SHOT352> ());
-		calculateDescriptors(cloud_filtered, keypoint, cloud_normals, descriptor);
-		cout << "Descriptor: " << descriptor->size();
-	
-		// Alineamos
-		visu_pc = cloud_filtered;
-		clouds.push_back(cloud_filtered);
-		keypoints.push_back(keypoint);
-		descriptors.push_back(descriptor);
-	}
-	if(align){
-		align = false;
-		if(!clouds.size() < 2){
-			Eigen::Matrix4f transformation;
-			pcl::PointCloud<pcl::PointXYZRGB>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-			for(unsigned i = 0;i<keypoints.size()-1;i++){
-				pcl::PointCloud<pcl::PointXYZRGB>::Ptr final (new pcl::PointCloud<pcl::PointXYZRGB>);
-				performAlign(keypoints[i]/*1*/, keypoints[i+1]/*2*/, descriptors[i]/*1*/, descriptors[i+1]/*2*/, final, transformation);
-				//pcl::transformPointCloud (*clouds[i], *transformed_cloud, transformation);
-				*visu_pc = *clouds[i]+*clouds[i+1];
-			}
-		}
+		clouds.push_back(cloud);
+		visu_pc = cloud;
 	}
 }
 
@@ -267,6 +73,7 @@ int main(int argc, char** argv){
 	ros::Subscriber sub = nh.subscribe<pcl::PointCloud<pcl::PointXYZRGB> >("/camera/depth/points", 1, callback);
 
 	boost::thread t(simpleVis);
+	std::cout << menu;
 	boost::thread t2(captureImage);
 
 	while(ros::ok()){
---
+++ catkin_ws/src/process_environtment/src/node.cpp
@@ -0,0 +1,240 @@
+#include <ros/ros.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <boost/foreach.hpp>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/keypoints/sift_keypoint.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/impl/point_types.hpp>
+#include <pcl/features/fpfh.h>
+#include <pcl/registration/icp.h>
+#include <pcl/registration/correspondence_rejection_sample_consensus.h>
+#include <pcl/keypoints/iss_3d.h>
+#include <pcl/correspondence.h>
+#include <vector>
+#include <fstream>
+#include <string>
+
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr visu_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
+std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr > clouds;
+std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr > filteredClouds;
+std::vector<Eigen::Matrix4f> transformations;
+
+void simpleVis (){
+  	pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
+
+	while(!viewer.wasStopped()){
+		viewer.showCloud (visu_pc);
+		boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
+	}
+}
+
+void estimateNormals(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud, pcl::PointCloud<pcl::PointNormal>::Ptr& cloud_normals){
+  	pcl::NormalEstimation<pcl::PointXYZRGB, pcl::PointNormal> ne;
+  	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree_n(new pcl::search::KdTree<pcl::PointXYZRGB>());
+
+ 	ne.setInputCloud(cloud);
+  	ne.setSearchMethod(tree_n);
+  	ne.setRadiusSearch(0.05);
+  	ne.compute(*cloud_normals);
+}
+
+void calculateKeyPoints(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud,
+	pcl::PointCloud<pcl::PointNormal>::Ptr& cloud_normals, pcl::PointCloud<pcl::PointWithScale>::Ptr& result){
+  	
+  	for(size_t i = 0; i<cloud_normals->points.size(); ++i)
+  	{
+    	cloud_normals->points[i].x = cloud->points[i].x;
+    	cloud_normals->points[i].y = cloud->points[i].y;
+    	cloud_normals->points[i].z = cloud->points[i].z;
+  	}
+  	pcl::SIFTKeypoint<pcl::PointNormal, pcl::PointWithScale> sift;
+  	
+ 	const static float min_scale = 0.01f; //0.01f
+  	const static int n_octaves = 3; //3
+  	const static int n_scales_per_octave = 4; //4
+  	const static float min_contrast = 0.001f; //.001f
+
+  	pcl::search::KdTree<pcl::PointNormal>::Ptr tree(new pcl::search::KdTree<pcl::PointNormal> ());
+  	sift.setSearchMethod(tree);
+  	sift.setScales(min_scale, n_octaves, n_scales_per_octave);
+  	sift.setMinimumContrast(min_contrast);
+  	sift.setInputCloud(cloud_normals);
+	sift.compute(*result);
+}
+
+void calculateFeatures(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud, pcl::PointCloud<pcl::PointWithScale>::Ptr& keypoints, 
+	pcl::PointCloud<pcl::PointNormal>::Ptr& cloud_normals, pcl::PointCloud<pcl::FPFHSignature33>::Ptr& fpfhs){
+	
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr keypoints_xyzrgb(new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::copyPointCloud (*keypoints, *keypoints_xyzrgb);
+	
+	pcl::FPFHEstimation<pcl::PointXYZRGB, pcl::PointNormal, pcl::FPFHSignature33> fpfh;
+  	fpfh.setInputCloud (keypoints_xyzrgb);
+  	fpfh.setInputNormals (cloud_normals);
+	fpfh.setSearchSurface (cloud);
+
+  	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB> ());
+	fpfh.setSearchMethod(tree);
+
+  	fpfh.setRadiusSearch (0.07);
+
+  	fpfh.compute (*fpfhs);
+}
+
+void matchFeatures(pcl::PointCloud<pcl::FPFHSignature33>::Ptr& source_features, pcl::PointCloud<pcl::FPFHSignature33>::Ptr& target_features,
+	pcl::Correspondences& correspondences){
+
+	pcl::registration::CorrespondenceEstimation<pcl::FPFHSignature33, pcl::FPFHSignature33> est;
+	est.setInputSource(source_features);
+	est.setInputTarget(target_features);
+	est.determineReciprocalCorrespondences (correspondences);
+}
+
+
+void getTransformation(pcl::PointCloud<pcl::PointWithScale>::Ptr& source, pcl::PointCloud<pcl::PointWithScale>::Ptr& target,
+	pcl::Correspondences& correspondences, Eigen::Matrix4f& transformation){
+	
+	pcl::registration::TransformationEstimationSVD<pcl::PointWithScale, pcl::PointWithScale> teSVD;
+	teSVD.estimateRigidTransformation(*source, *target, correspondences, transformation);
+}
+
+Eigen::Matrix4f correspondencesRejection(pcl::PointCloud<pcl::PointWithScale>::Ptr& keypoints_1, pcl::PointCloud<pcl::PointWithScale>::Ptr& keypoints_2,
+	pcl::Correspondences& correspondences, pcl::Correspondences& correspondences_out){ 
+	
+	pcl::CorrespondencesConstPtr correspondences_p(new pcl::Correspondences(correspondences));
+
+    pcl::registration::CorrespondenceRejectorSampleConsensus<pcl::PointWithScale> sac;
+    sac.setInputSource(keypoints_1); // reverse? (2 for 1)
+    sac.setInputTarget(keypoints_2); // reverse (1 for 2) 
+    sac.setInlierThreshold(0.025); // Too much? default 0.01 
+    sac.setMaximumIterations(10000); //default 1000 
+    sac.setRefineModel(true); // default false 
+    sac.setInputCorrespondences(correspondences_p); 
+    sac.getCorrespondences(correspondences_out);
+    return sac.getBestTransformation();
+
+}
+
+void filterCloudPoint(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& filtered){
+	pcl::VoxelGrid<pcl::PointXYZRGB > vGrid;
+	vGrid.setInputCloud (cloud);
+	vGrid.setLeafSize (0.025f, 0.025f, 0.025f);
+	vGrid.filter (*filtered);
+}
+
+
+void processAllFiles(){
+	for(unsigned i=0;i<clouds.size()-1;i++){
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered_left (new pcl::PointCloud<pcl::PointXYZRGB>);
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered_right (new pcl::PointCloud<pcl::PointXYZRGB>);
+  		pcl::PointCloud<pcl::PointNormal>::Ptr cloud_normals_left (new pcl::PointCloud<pcl::PointNormal>);
+  		pcl::PointCloud<pcl::PointNormal>::Ptr cloud_normals_right (new pcl::PointCloud<pcl::PointNormal>);
+		pcl::PointCloud<pcl::PointWithScale>::Ptr keypoints_left (new pcl::PointCloud<pcl::PointWithScale> ());
+		pcl::PointCloud<pcl::PointWithScale>::Ptr keypoints_right (new pcl::PointCloud<pcl::PointWithScale> ());
+  		pcl::PointCloud<pcl::FPFHSignature33>::Ptr fpfhs_left (new pcl::PointCloud<pcl::FPFHSignature33> ());
+  		pcl::PointCloud<pcl::FPFHSignature33>::Ptr fpfhs_right (new pcl::PointCloud<pcl::FPFHSignature33> ());
+		pcl::Correspondences correspondences;
+		pcl::Correspondences correspondencesRejected;
+		
+		// Filtramos los puntos
+		filterCloudPoint(clouds[i], cloud_filtered_left);
+		filterCloudPoint(clouds[i+1], cloud_filtered_right);
+		
+		// Conseguimos las normales
+  		estimateNormals(cloud_filtered_left, cloud_normals_left);
+  		estimateNormals(cloud_filtered_right, cloud_normals_right);
+
+		// Sacamos puntos característicos
+		calculateKeyPoints(cloud_filtered_left, cloud_normals_left, keypoints_left);
+		calculateKeyPoints(cloud_filtered_right, cloud_normals_right, keypoints_right);
+
+		// Calculamos features de los keypoints		
+  		calculateFeatures(cloud_filtered_left, keypoints_left, cloud_normals_left, fpfhs_left);
+  		calculateFeatures(cloud_filtered_right, keypoints_right, cloud_normals_right, fpfhs_right);
+
+  		// Buscamos las correspondencias
+  		matchFeatures(fpfhs_left, fpfhs_right, correspondences);
+
+
+  		
+  		// Filtramos las correspondencias
+  		// Obtenemos la matriz de transformación
+  		Eigen::Matrix4f transformation = correspondencesRejection(keypoints_left, keypoints_right, correspondences, correspondencesRejected);
+
+
+  		cout << "\033[1;33mProgress: " << i << "/" << clouds.size()-2 << "\033[0m" << endl;
+  		cout << "\tCloud \033[1;35mleft\033[0m points in cloud after VG: \033[1;35m" << (*cloud_filtered_left).size() << "\033[0m" << endl;
+  		cout << "\tCloud \033[1;35mleft\033[0m normal points in cloud: \033[1;35m" << (*cloud_normals_left).size() << "\033[0m" << endl;
+  		cout << "\tCloud \033[1;35mleft\033[0m keypoints: \033[1;35m" << (*keypoints_left).size() << "\033[0m" << endl;
+  		cout << "\tCloud \033[1;35mleft\033[0m descriptors: \033[1;35m" << (*fpfhs_left).size() << "\033[0m" << endl;
+  		cout << "\tCloud \033[1;36mright\033[0m points in cloud after VG: \033[1;36m" << (*cloud_filtered_right).size() << "\033[0m" << endl;
+  		cout << "\tCloud \033[1;36mright\033[0m normal points in cloud: \033[1;36m" << (*cloud_normals_right).size() << "\033[0m" << endl;
+  		cout << "\tCloud \033[1;36mright\033[0m keypoints: \033[1;36m" << (*keypoints_right).size() << "\033[0m" << endl;
+  		cout << "\tCloud \033[1;36mright\033[0m descriptors: \033[1;36m" << (*fpfhs_right).size() << "\033[0m" << endl;
+  		cout << "------------------------------------------------" << endl;
+  		cout << "\033[1;32mCorrespondences found: " << correspondences.size() << endl;
+  		cout << "\033[1;31mCorrespondences rejected: " << correspondencesRejected.size() << "\033[0m" << endl;
+  		cout << "------------------------------------------------" << endl << endl;
+  		
+  		//getTransformation(keypoints_left, keypoints_right, correspondences, transformation);
+		transformations.push_back(transformation);
+		filteredClouds.push_back(cloud_filtered_left);
+		if(i==clouds.size()-2){
+			filteredClouds.push_back(cloud_filtered_right);
+		}
+	}
+}
+
+void alignAll(){
+	cout << "Aligning..." << endl;
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr transformed_cloud_out (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::transformPointCloud(*filteredClouds[0], *transformed_cloud_out, transformations[0]);
+	*visu_pc = *transformed_cloud_out+*filteredClouds[1];
+	cout << "\033[1;33m\t" << 0 << setw(2) << "/ " << transformations.size()-1 << "\r\033[0m" << std::flush;
+	for(unsigned int i=1;i<transformations.size();i++){
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr transformed_cloud_inside (new pcl::PointCloud<pcl::PointXYZRGB>);
+    	pcl::transformPointCloud (*visu_pc, *transformed_cloud_inside, transformations[i]);
+		*visu_pc = *filteredClouds[i+1]+*transformed_cloud_inside;
+		cout << "\033[1;33m\t" << i << setw(2) << "/ " << transformations.size()-1 << "\r\033[0m" << std::flush;
+	}
+	cout << endl;
+	cout << "\033[1;31mAligning Done.\033[0m" << endl;
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr final (new pcl::PointCloud<pcl::PointXYZRGB>);
+	filterCloudPoint(visu_pc, final);
+	visu_pc = final;
+}
+
+
+bool loadPointClouds(){
+	std::ifstream f;
+	std::string fileName;
+	f.open("listOfPointClouds.txt");
+	if(f.is_open()){
+		while(std::getline(f, fileName)){
+			pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+			if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (fileName, *cloud) == -1){
+				cout << "Couldn't read file " << fileName << "\n";
+				clouds.clear();
+				return false;
+			}
+			clouds.push_back(cloud);
+		}
+	}
+	f.close();
+	return true;
+}
+
+int main(int argc, char** argv){
+	
+	if(loadPointClouds()){
+		processAllFiles();
+		alignAll();
+		simpleVis();
+	}
+
+	return -1;
+}
